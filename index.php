502098eb8a440b4d8613b50e6312d813f33cdabe
RyuuBehemoth <alejandrokapd@gmail.com>
2023-04-13 07:15:35 -0500

    php files changed

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     $firstNumber = 2;
     $secondNumber = 4;
     echo $firstNumber + $secondNumber;
+    echo $firstNumber - $secondNumber;
+    echo $firstNumber * $secondNumber;
+    echo $firstNumber / $secondNumber;
 ?>
